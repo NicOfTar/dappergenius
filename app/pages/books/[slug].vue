ba34a0d8908c694c68268587e1e168c030381034
Troy Williams <dappergenius@outlook.com>
2025-06-19 14:15:11 -0500

    books is working

--- app/pages/books/[slug].vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="container mx-auto py-8">
-    <ContentDoc v-slot="{ doc }">
-      <h1 class="text-3xl font-bold mb-4">{{ doc.title || doc._file }}</h1>
-      <ContentRenderer :value="doc" />
-    </ContentDoc>
-  </div>
-</template>
